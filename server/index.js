363c94f07f0a76ddf8fe5b3fdde3874eb88ba3d8
gautham210 <gauthamkrishna02102007@gmail.com>
2026-04-20 20:05:43 +0530

    fix: manual CORS headers for all requests

--- server/index.js
+++ server/index.js
@@ -1,17 +1,20 @@
 require("dotenv").config();
 const express = require('express');
-const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-app.options('*', cors());
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 
 app.use(express.json());
 
